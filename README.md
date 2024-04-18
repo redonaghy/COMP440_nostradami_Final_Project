b75814aad715d4bceee4eaa2645d7f4be9dc0a40
Rory Donaghy <rdonaghy@macalester.edu>
2024-04-17 20:03:25 -0500

    Seperated CSV Creation Into Its Own Python Script Also Updated README with setup instructions && gitignore for csv files.

--- README.md
+++ README.md
@@ -1,5 +1,16 @@
 # COMP440_nostradami_Final_Project
 
+## Project goal
+
+## To use this project, you'll need:
+
+a working `python3` installation, `jupyterlab`, `ipython`
+
+jupyter and ipython can be installed with pip. 
+
+If you're having trouble running pip executibles, check out the second response
+of [this stack overflow thread](https://stackoverflow.com/questions/35898734/pip-installs-packages-successfully-but-executables-not-found-from-command-line).
+
 ## Environment setup:
 
 Place `kaggle.json` in '~/.kaggle' directory, optionally adjust the permissions 
@@ -26,3 +37,13 @@ Finally, sync project requirements with
 ```bash
 pip install -r requirements.txt
 ```
+
+Now that our virtual environment is configured, we can create a kernel for 
+jupyter to use with ipython. That can be done with 
+
+```bash
+ipython kernel install --user --name=venv
+```
+
+At this point you should now be able to select the venv as a jupyter runtime 
+environment and you're good to go!
---
+++ clean_data.py
@@ -0,0 +1,65 @@
+from kaggle.api.kaggle_api_extended import KaggleApi
+from zipfile import ZipFile 
+from ijson import items
+
+import pandas as pd
+import numpy as np
+import os
+
+# Setup Kaggle API To Stream Dataset
+api = KaggleApi()
+api.authenticate()
+
+# Initialize Lists To Store Data
+ids, titles, years, n_citations, doc_types, authors_list, venues, fos = [], [], [], [], [], [], [], []
+
+# DEBUG Stop Processing Objects After X Objects
+MAX_OBJECTS = 1000
+
+# Grab Dataset and Place In CWD
+api.dataset_download_files('mathurinache/citation-network-dataset')
+data_zf = ZipFile('citation-network-dataset.zip', 'r')
+
+# Takes Values From JSON Entry and Appends Them To Colums
+def process_json_obj(obj):
+    ids.append(obj.get('id'))
+    titles.append(obj.get('title'))
+    years.append(obj.get('year'))
+    n_citations.append(obj.get('n_citation'))
+    doc_types.append(obj.get('doc_type'))
+    authors_names = ", ".join([author.get('name') for author in obj.get('authors', [])])
+    authors_list.append(authors_names)
+    venues.append(obj.get('venue', {}).get('raw', ''))
+    tmp = []
+    for field in obj.get("fos", []):
+        tmp.append(field.get("name"))
+    fos.append(', '.join(map(str, tmp)))
+
+# Create Columns From JSON Data
+with data_zf:
+    with data_zf.open('dblp.v12.json') as json:
+        objects = items(json, 'item')
+        for i, obj, in enumerate(objects):
+            # DEBUG
+            if i >= MAX_OBJECTS:
+                break
+            process_json_obj(obj)
+
+# Create a DataFrame From Our Columns
+citations_df = pd.DataFrame({
+    'ID': ids,
+    'Title': titles,
+    'Year': years,
+    'Citations': n_citations,
+    'Document Type': doc_types,
+    'Authors': authors_list,
+    'Venue': venues,
+    'Field of Study': fos
+})
+
+# Write DF To CSV
+with open("data.csv", "x") as csv:
+  csv.write(citations_df.to_csv())
+
+os.remove('citation-network-dataset.zip')
+
